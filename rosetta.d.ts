7920b97623c22c957cfcbf65141f54c5efc3b886
Jacob Fowles <jacob@underbelly.is>
2020-05-16 00:58:35 -0600

    fix(type): replace unknown `array` type (#5)
    * Update type definitions
    
    `array` is not a valid Typescript type. Moved to using `Array<T>` instead and updated `object` to `Record<string, any>`
    
    * fix: use `any` instead of `T` type
    
    Co-authored-by: Luke Edwards <luke.edwards05@gmail.com>

--- rosetta.d.ts
+++ rosetta.d.ts
@@ -4,7 +4,7 @@ export interface Rosetta<T> {
 	/** Define or extend the language table */
 	set(lang: string, table: T): void;
 	/** Retrieve a translation segment for the current language */
-	t<X extends object | array>(key: string | (string | number)[], params?: X, lang?: string): string;
+	t<X extends Record<string, any> | any[]>(key: string | (string | number)[], params?: X, lang?: string): string;
 }
 
 export default function<T>(dict?: Record<string, T>): Rosetta<T>;
