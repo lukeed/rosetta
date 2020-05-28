4d3a6b095327251ce279b3e6c0c06041ff552b98
Jacob Fowles <jacob@underbelly.is>
2020-05-28 12:36:38 -0600

    Support Internet Explorer
    Internet Explorer does not support implicit field names for object methods.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ export default function (obj) {
 	var locale='', tree = obj || {};
 
 	return {
-		set(lang, table) {
+		set: function (lang, table) {
 			tree[lang] = Object.assign(tree[lang] || {}, table);
 		},
 
-		locale(lang) {
+		locale: function (lang) {
 			locale = lang;
 		},
 
-		t(key, params, lang) {
+		t: function (key, params, lang) {
 			var val = dlv(tree[lang || locale], key, '');
 			if (typeof val === 'function') return val(params);
 			if (typeof val === 'string') return tmpl(val, params);
