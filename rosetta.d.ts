313501396d63467d1d548c3274687d96abe44d4d
Luke Edwards <luke.edwards05@gmail.com>
2020-06-29 14:09:32 -0700

    chore: update type declaration format

--- rosetta.d.ts
+++ rosetta.d.ts
@@ -1,10 +1,27 @@
-export interface Rosetta<T> {
-	/** Get/Set the language key */
-	locale(lang?: string): string;
-	/** Define or extend the language table */
-	set(lang: string, table: T): void;
-	/** Retrieve a translation segment for the current language */
-	t<X extends Record<string, any> | any[]>(key: string | (string | number)[], params?: X, lang?: string): string;
+declare module 'rosetta' {
+	export interface Rosetta<T> {
+		/** Get/Set the language key */
+		locale(lang?: string): string;
+		/** Define or extend the language table */
+		set(lang: string, table: T): void;
+		/** Retrieve a translation segment for the current language */
+		t<X extends Record<string, any> | any[]>(key: string | (string | number)[], params?: X, lang?: string): string;
+	}
+
+	function rosetta<T>(dict?: Record<string, T>): Rosetta<T>;
+	export = rosetta;
 }
 
-export default function<T>(dict?: Record<string, T>): Rosetta<T>;
+declare module 'rosetta/debug' {
+	export interface Rosetta<T> {
+		/** Get/Set the language key */
+		locale(lang?: string): string;
+		/** Define or extend the language table */
+		set(lang: string, table: T): void;
+		/** Retrieve a translation segment for the current language */
+		t<X extends Record<string, any> | any[]>(key: string | (string | number)[], params?: X, lang?: string): string;
+	}
+
+	function rosetta<T>(dict?: Record<string, T>): Rosetta<T>;
+	export = rosetta;
+}
